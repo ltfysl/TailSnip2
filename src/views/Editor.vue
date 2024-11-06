4ec1fe73331152032ba043aa3f82b718d74927b5
ltfysl <yesilatif@gmail.com>
2024-11-06 20:43:01 +0100

    Make Editor sidebar collapsable

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -2,12 +2,34 @@
   <div class="flex h-full w-full">
     <!-- Left Sidebar -->
     <div
-      class="flex w-64 flex-shrink-0 flex-col border-r border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800"
+      class="flex flex-col border-r border-gray-200 bg-white transition-all duration-300 dark:border-gray-700 dark:bg-gray-800"
+      :class="[sidebarCollapsed ? 'w-12' : 'w-64']"
     >
-      <div class="flex-1 overflow-y-auto">
+      <div
+        class="flex items-center justify-between border-b border-gray-200 p-4 dark:border-gray-700"
+      >
+        <h2
+          class="text-lg font-semibold"
+          :class="{ 'sr-only': sidebarCollapsed }"
+        >
+          Components
+        </h2>
+        <button
+          @click="toggleSidebar"
+          class="rounded-md p-1 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
+          :title="sidebarCollapsed ? 'Expand sidebar' : 'Collapse sidebar'"
+        >
+          <ChevronLeftIcon v-if="!sidebarCollapsed" class="h-5 w-5" />
+          <ChevronRightIcon v-else class="h-5 w-5" />
+        </button>
+      </div>
+      <div
+        class="flex-1 overflow-y-auto"
+        :class="{ 'sr-only': sidebarCollapsed }"
+      >
         <ComponentLibrary />
       </div>
-      <ComponentVersions v-if="store.activeComponent" />
+      <ComponentVersions v-if="store.activeComponent && !sidebarCollapsed" />
     </div>
 
     <!-- Middle Section - Monaco Editor -->
@@ -25,6 +47,8 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/24/outline';
 import { useComponentStore } from '../stores/components';
 import ComponentLibrary from '../components/ComponentLibrary.vue';
 import ComponentVersions from '../components/ComponentVersions.vue';
@@ -32,4 +56,23 @@ import CodeEditor from '../components/CodeEditor.vue';
 import PreviewPanel from '../components/PreviewPanel.vue';
 
 const store = useComponentStore();
+const sidebarCollapsed = ref(false);
+
+const toggleSidebar = () => {
+  sidebarCollapsed.value = !sidebarCollapsed.value;
+};
 </script>
+
+<style scoped>
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+}
+</style>
